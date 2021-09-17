afa2091e1c465f519f87cd928c148e03e9278a5a
Bryan Ortuno <bryan.ortuno@epn.edu.ec>
2021-09-17 10:22:34 -0500

    xx

--- src/App.js
+++ src/App.js
@@ -75,36 +75,7 @@ function App() {
   const handleClose = () => {
     setOpen(false);
   };
-  /*
-  const validarCedula = (value) => {
-    let error;
-    var cad = value.trim();
-    var total = 0;
-    var longitud = cad.length;
-    var longcheck = longitud - 1;
 
-    if (cad !== "" && longitud === 10) {
-      for (var i = 0; i < longcheck; i++) {
-        if (i % 2 === 0) {
-          var aux = cad.charAt(i) * 2;
-          if (aux > 9) aux -= 9;
-          total += aux;
-        } else {
-          total += parseInt(cad.charAt(i)); // parseInt o concatenará en lugar de sumar
-        }
-      }
-
-      total = total % 10 ? 10 - (total % 10) : 0;
-
-      if (cad.charAt(longitud - 1) === total) {
-        console.log("valida");
-      } else {
-        console.log("no valida");
-      }
-    }
-    return error;
-  };
-*/
   return (
     <div className="App">
       <a href="#content" role="navigation">
